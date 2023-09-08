f0c8aedac080a701403c567f8a50cc38de2d55a6
toluwalopeogunniyi@gmail.com <toluwalopeogunniyi@gmail.com>
2023-09-08 16:39:24 -0700

    5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Write a program that prints the alphabet in lowercase,
+* followed by a new line.
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != e && letter != q)
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
