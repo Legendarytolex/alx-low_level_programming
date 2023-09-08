516930e29f57b00e38196ebcd1a4cce3694c2eb4
toluwalopeogunniyi@gmail.com <toluwalopeogunniyi@gmail.com>
2023-09-08 15:49:36 -0700

    third task is done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * a loop that prints all the alphabets in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar(\n);
+	return (0);
+}
