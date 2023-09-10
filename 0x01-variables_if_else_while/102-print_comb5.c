baaf2523e0335fda3d474f3628f5ba748858b111
toluwalopeogunniyi@gmail.com <toluwalopeogunniyi@gmail.com>
2023-09-10 04:09:21 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,7 +4,6 @@
  * a program that prints all possible combinations of two two-digit numbers.
  * Return: always 0 (success)
  */
-
 int main(void)
 {
 	int tens1 = 0;
@@ -19,18 +18,21 @@ int main(void)
 		{
 			while (ones2 <= 9)
 			{
+				if (tens1 == 0 && ones1 == 0)
+				{
+					ones2++;
+					continue;
+				}
 				putchar('0' + tens1);
 				putchar('0' + ones1);
 				putchar(' ');
 				putchar('0' + tens2);
 				putchar('0' + ones2);
-
 				if (tens1 < 9 || ones1 < 9 || tens2 < 9 || ones2 < 9)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-
 				ones2++;
 			}
 				ones2 = 0;
@@ -43,7 +45,6 @@ int main(void)
 			tens1++;
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
