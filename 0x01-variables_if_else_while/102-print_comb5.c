ca92440861f2745706f146dee9fe3770d4855a0b
toluwalopeogunniyi@gmail.com <toluwalopeogunniyi@gmail.com>
2023-09-10 11:12:15 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 
 	while (i <= 98)
 	{
-		j = 1;
+		j = i + 1;
 
 		while (j <= 99)
 		{
@@ -22,7 +22,7 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 
-			if (i != 99 || j != 99)
+			if (i != 98 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
