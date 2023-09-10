7358458b1d171e993e63a2e607aa4925e220e98c
toluwalopeogunniyi@gmail.com <toluwalopeogunniyi@gmail.com>
2023-09-10 02:28:01 -0700

    9 done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - entry point
+*write a program that prints all
+*possible combinations of single-digit numbers.
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar('0' + digit);
+		putchar(',');
+		putchar(' ');
+		digit++;
+	}
+
+	putchar('\n');
+	return (0);
+}
