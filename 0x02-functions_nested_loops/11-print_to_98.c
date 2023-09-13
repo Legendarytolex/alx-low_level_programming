ecb1a667279cef3d74d7510a373e3c3ad96df300
legendarytolex <toluwalopeogunniyi@gmail.com>
2023-09-12 17:22:38 -0700

    done with first section of task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - a function that prints all natural numbers
+ * from n to 98, followed by a new line.
+ *
+ * @n: the integer to be checked
+ *
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		int i;
+
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		int i;
+
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+
+	printf("\n");
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,7 @@
  *
  * Return: the value of the last digit
  */
-int print_last_digit(int n)
+int print_last_digit(int)
 {
 	int last_digit;
 
