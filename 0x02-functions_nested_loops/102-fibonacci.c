0f07834d85a52bf691d252f14584aec471a1bfb6
legendarytolex <toluwalopeogunniyi@gmail.com>
2023-09-12 18:26:33 -0700

    102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,18 +8,18 @@
 int main(void)
 {
 	int n = 50;
-	long long int fib1 = 1;
-	long long int fib2 = 2;
-	long long int next;
+	long int fib1 = 1;
+	long int fib2 = 2;
+	long int next;
 	int count = 0;
 
-	printf("lld, %lld", fib1, fib2);
+	printf("ld, %ld", fib1, fib2);
 	count = 2;
 
 	while (count < n)
 	{
 		next = fib1 + fib2;
-		printf(", %lld", next);
+		printf(", %ld", next);
 
 		fib1 = fib2;
 		fib2 = next;
