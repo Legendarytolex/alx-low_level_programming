baa090798e3fa6d7eaf1eb07a694d8951929a40b
legendarytolex <toluwalopeogunniyi@gmail.com>
2023-09-22 03:05:55 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,7 +8,7 @@
  * @n: variable of int type
  * Return: Return a pointer to the resulting string dest
  */
-char *_strncpy(char *dest,char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	char *result = dest;
 
