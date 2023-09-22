48f4820b276daf88248555526893a63b43e6511a
legendarytolex <toluwalopeogunniyi@gmail.com>
2023-09-22 02:48:40 -0700

    task 2 corrected

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,19 +12,21 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	char *result = dest;
 
-	while (src != '\0' && n > 0)
+	while (n > 0)
 	{
-		*dest = *src;
+		if (src != '\0')
+		{
+			*dest = *src;
+			dest++;
+			src++;
+		}
+		else
+		{
+			*dest = '\0';
+		}
 		dest++;
-		src++;
 		n--;
 	}
 
-	while (n > 0)
-	{
-		*dest = '\0';
-		dest++;
-		n--;
-	}
 	return (result);
 }
