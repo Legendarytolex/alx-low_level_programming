93854160bdbcad1967be5a564d1a76e57cfad115
legendarytolex <toluwalopeogunniyi@gmail.com>
2023-10-02 13:22:58 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - a program that prints its name, followed by a new line
+ *
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		_putchar("s\n", argv[0]);
+	}
+	return(0);
+}
