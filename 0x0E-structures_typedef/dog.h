1fadd23075ea16f35dcc4ae1d38f0a5be788ac02
legendarytolex <toluwalopeogunniyi@gmail.com>
2023-10-10 16:35:29 -0700

    corrected task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+/**
+ * struct dog - the name of the struct describing the name, age and owner
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner
+ *
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
