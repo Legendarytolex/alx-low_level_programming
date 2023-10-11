b02c712dd2b87d62ae707eced95b7c8f76265b9e
legendarytolex <toluwalopeogunniyi@gmail.com>
2023-10-10 17:22:33 -0700

    1 done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 /**
  * struct dog - the name of the struct describing the name, age and owner
  * @name: the name of the dog
@@ -21,7 +27,5 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
-
-typedef struct dog dog_t;
 
 #endif
